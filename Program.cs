a4ad1296a888844b8e5ed23420d820b8e40213b1
NatiSoriaa <naticsoria@gmail.com>
2023-11-28 20:27:26 +0100

    Codigo Csharp

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int Number;
+
+        Console.WriteLine("escribe un numero");
+        Number = Convert.ToInt32(Console.ReadLine());
+        if (Number % 2 == 0){
+            Console.WriteLine("El numero es par");
+        }
+        else{
+            Console.WriteLine("El numero es impar");
+        }
+
+    }
+}
